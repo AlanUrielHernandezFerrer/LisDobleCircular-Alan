1b735ff20e4b5fb368f329866e5269733ebbfe3d
AlanUrielHernandezFerrer <alan.ferrer3212@gmail.com>
2019-10-27 13:12:39 -0600

    Actualido
    Trabajo acutualizado ya quedo

---
+++ ListaDobleCircular/src/listadoblecircular/nodo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listadoblecircular;
+
+/**
+ *
+ * @author Alan
+ */
+public class nodo {
+ int dato;
+ nodo next;
+ nodo ant;
+ nodo(int a)
+ {
+  dato=a;
+  ant=null;
+  next=null;
+ }
+}
